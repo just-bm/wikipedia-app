0314d7e584cef5796dda30e78b9e577e2df80570
just-bm <tbalancemurugan>
2025-08-16 01:06:54 +0530

    lsdjffkldflkdsjf

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -14,7 +14,7 @@ export default function App(){
     setError("");
     setLoading(true);
     try{
-      const response = await fetch(`https://wikipedia-app.onrender.com/=${encodeURIComponent(query)}`);
+      const response = await fetch(`https://wikipedia-app.onrender.com/search?q=${encodeURIComponent(query)}`);
 
       const data = await response.json();
       setResults(data);
